b5ee4c9e5620fa53691739b17e5723294c353580
Ayush <7126924ak@gmail.com>
2022-12-07 18:29:27 +0530

    added sound and retey property

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-document.querySelector("#text-win-lose").style.display = "none";
-
 var box = ["paper", "scissor", "rock"];
 var userChoice = document.querySelectorAll(".forUser");
 var comChoice = document.querySelectorAll(".forCom");
+
+var score = 0;
 
 var started = false;
 // for pop up 
@@ -26,27 +26,25 @@ document.querySelector("#overlayer").addEventListener("click", function () {
 });
 
 
-document.querySelector(".btn").addEventListener("click", function () {
-
-    console.log("BTTON");
-    for (let i = 0; i < 3; i++) {
-        userChoice[i].classList.remove("disabled");
-
-        userChoice[i].classList.add("circle-size-1");
-        userChoice[i].classList.add(box[i] + "-pos");
-        document.querySelectorAll(".icon-img-1")[i].classList.add("icon-img");
-    }
-    document.querySelector(".svg-bg").style.display = "block";
-
+document.querySelector("#text-win-lose .btn").addEventListener("click", function () {
 
     document.querySelector("#mid-area").classList.remove("step-3");
     document.querySelector("#mid-area").classList.add("step-1");
+    for (let i = 0; i < 3; i++) {
+        userChoice[i].classList.remove("disabled");
+        userChoice[i].classList.remove("circle-size-2");
+        document.querySelectorAll(".icon-img")[i].classList.remove("icon-img-2");
 
+        userChoice[i].classList.add("circle-size-1");
+        userChoice[i].classList.add(box[i] + "-pos");
+
+        comChoice[i].classList.add("disabled");
+    }
+
+    document.querySelector(".svg-bg").style.display = "block";
+    document.querySelector("#text-win-lose").classList.add("disabled");
     document.querySelector(".who-picked").classList.add("disabled");
-    // document.querySelector("#" + value).classList.remove("disabled");
-    // document.querySelector("#" + value).classList.add("circle-size-2");
-
-    // document.querySelector(".dark-cir").classList.remove("disabled");
+    started = false;
 
 });
 
@@ -118,16 +116,21 @@ function comPicked() {
 }
 
 function checkWinner(value1, value2) {
-    document.querySelector("#mid-area").classList.remove("step-2");
-    document.querySelector(".who-picked").style.width = "80%";
-    document.querySelector("#mid-area").classList.add("step-3");
 
     setTimeout(function () {
-        document.querySelector("#text-win-lose").style.display = "block";
+        document.querySelector("#mid-area").classList.remove("step-2");
+        document.querySelector(".who-picked").style.width = "80%";
+        document.querySelector("#mid-area").classList.add("step-3");
+
+        document.querySelector("#text-win-lose").classList.remove("disabled");
 
         if ((value1 === "paper" && value2 == "rock") || (value1 === "rock" && value2 == "scissor") || (value1 === "scissor" && value2 == "paper")) {
             console.log("you win");
+            playSound("wow");
+            score++;
             document.querySelector(".result-text").innerHTML = "YOU WIN";
+            document.querySelector("#score").innerHTML = score;
+
         }
         else if (value1 === value2) {
             document.querySelector(".result-text").innerHTML = "DRAW";
@@ -135,7 +138,13 @@ function checkWinner(value1, value2) {
         }
         else {
             console.log("you lose");
+            playSound("lose");
             document.querySelector(".result-text").innerHTML = "YOU LOSE";
         }
     }, 1500);
 }
+
+function playSound(name) {
+    var sound = new Audio("Sounds/" + name + ".mp3");
+    sound.play();
+  }
